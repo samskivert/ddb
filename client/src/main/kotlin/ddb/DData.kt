3ac2e6b0edc3cb3bc3c730ebea4810c6cfe50c0c
Michael Bayne <mdb@samskivert.com>
2015-11-09 18:00:14 -0800

    A marker interface for szable data classes.

---
+++ client/src/main/kotlin/ddb/DData.kt
@@ -0,0 +1,9 @@
+//
+// DDB - for great syncing of data between server and clients
+
+package ddb
+
+/**
+ * Marker trait for types which will be transmissible between client and server.
+ */
+interface DData {}
